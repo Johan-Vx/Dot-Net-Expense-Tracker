e290ed089f42e01488bc4550d1c5023f0d5d5723
Johan Vx <tohongvu10a5@gmail.com>
2026-05-11 10:38:35 +0700

    refactor(viewmodel): replace CloseAction with DialogResult binding in MainViewModel and LoginViewModel for proper MVVM

---
+++ Expense_Tracker/Helper/WindowBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace Expense_Tracker.Behavior
+{
+    public static class WindowBehavior
+    {
+        public static readonly DependencyProperty DialogResultProperty =
+            DependencyProperty.RegisterAttached(
+                "DialogResult",
+                typeof(bool?),
+                typeof(WindowBehavior),
+                new PropertyMetadata(null, OnDialogResultChanged));
+        public static void SetDialogResult(DependencyObject d, bool? value)
+        {
+            d.SetValue(DialogResultProperty, value);
+        }
+        public static bool? GetDialogResult(DependencyObject d)
+        {
+            return (bool?)d.GetValue(DialogResultProperty);
+        }
+        private static void OnDialogResultChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Window window)
+            {
+                if (e.NewValue != null)
+                {
+                    try
+                    {
+                        window.DialogResult = e.NewValue as bool?;
+                    }
+                    catch
+                    {
+                        window.Close();
+                    }
+                }
+            }
+        }
+    }
+}
