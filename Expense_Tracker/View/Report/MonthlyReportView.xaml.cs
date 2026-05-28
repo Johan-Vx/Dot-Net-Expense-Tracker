d049310975a1dafb045556f6b079b3532f56896a
Johan Vx <tohongvu10a5@gmail.com>
2026-05-28 23:03:42 +0700

    feat(report): implement monthly report view

---
+++ Expense_Tracker/View/Report/MonthlyReportView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Expense_Tracker.View
+{
+    /// <summary>
+    /// Interaction logic for MonthlyReportView.xaml
+    /// </summary>
+    public partial class MonthlyReportView : UserControl
+    {
+        public MonthlyReportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
